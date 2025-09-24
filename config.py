02fc9a1484597586f8f136e0f684cb1de4a21117
Aayush Shrestha <aashr3@morgan.edu>
2025-09-24 18:52:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ NAO_IP   = os.environ.get("NAO_IP", "172.20.95.105")
 NAO_PORT = int(os.environ.get("NAO_PORT", "9559"))
 
 # OpenAI settings
-# Make sure you export OPENAI_API_KEY before running:
-#    export OPENAI_API_KEY=sk-…
+
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 MODEL_PROVIDER = os.environ.get("MODEL_PROVIDER", "openai")
 
